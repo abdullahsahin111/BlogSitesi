821b546c7d5746f2802d66950959cd846ad004d4
abdullah.sahin@isper.istanbul <abdullah.sahin@isper.istanbul>
2023-10-24 23:10:55 +0300

    Ders 8

---
+++ CoreDemo/EntityLayer/Concrete/Comment.cs
@@ -0,0 +1,12 @@
+﻿namespace EntityLayer.Concrete
+{
+    public class Comment
+    {
+        public int CommentID { get; set; }
+        public string CommentUserName { get; set; }
+        public string CommentTitle { get; set; }
+        public string CommentContent { get; set; }
+        public DateTime CommentDate { get; set; }
+        public bool CommentStatus { get; set; }
+    }
+}
