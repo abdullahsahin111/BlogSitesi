16e0681f921ac0bf267d74e0de21f4ee7cb071e3
abdullah.sahin@isper.istanbul <abdullah.sahin@isper.istanbul>
2023-10-25 18:52:31 +0300

    11 Asp.Net Core 5.0 Proje Kampı Referansların Tanımlanması Ve Context Dbset Propertyleri

--- CoreDemo/DataAccessLayer/Concrete/Context.cs
+++ CoreDemo/DataAccessLayer/Concrete/Context.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using EntityLayer.Concrete;
+using Microsoft.EntityFrameworkCore;
 
 namespace DataAccessLayer.Concrete
 {
@@ -8,5 +9,11 @@ namespace DataAccessLayer.Concrete
         {
             optionsBuilder.UseSqlServer("Provider=SQLNCLI10;Server=94.73.170.34;Database=u1447334_dbCore; Uid=u1447334_user291;Pwd=sx23VwTx5GtKqqi;");
         }
+        public DbSet<About> Abouts { get; set; }
+        public DbSet<Blog> Blogs { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Comment> Comments { get; set; }
+        public DbSet<Context> Contexts { get; set; }
+        public DbSet<Writer> Writers { get; set; }
     }
 }
