01a82b6fd37f133febb6d3c88038db8349cc0b4f
abdullah.sahin@isper.istanbul <abdullah.sahin@isper.istanbul>
2023-10-25 18:42:02 +0300

    Ders: 10 Asp.Net Core 5.0 Proje Kampı Connection String Ve Context Sınıfı

---
+++ CoreDemo/DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace DataAccessLayer.Concrete
+{
+    public class Context : DbContext
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Provider=SQLNCLI10;Server=94.73.170.34;Database=u1447334_dbCore; Uid=u1447334_user291;Pwd=sx23VwTx5GtKqqi;");
+        }
+    }
+}
