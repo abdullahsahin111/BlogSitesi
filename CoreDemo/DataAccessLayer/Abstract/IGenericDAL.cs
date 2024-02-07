33e0fe610ad5b84431dec46939ee1d92eecfec92
abdullah.sahin@isper.istanbul <abdullah.sahin@isper.istanbul>
2024-02-07 13:24:10 +0300

    Repository eklendi

---
+++ CoreDemo/DataAccessLayer/Abstract/IGenericDAL.cs
@@ -0,0 +1,11 @@
+﻿namespace DataAccessLayer.Abstract
+{
+    public interface IGenericDAL<T> where T : class
+    {
+        void Insert(T t);
+        void Delete(T t);
+        void Update(T t);
+        List<T> GetListAll();
+        T GetById(int id);
+    }
+}
---
+++ CoreDemo/DataAccessLayer/Repositories/CommentRepository.cs
@@ -0,0 +1,33 @@
+﻿using DataAccessLayer.Abstract;
+using EntityLayer.Concrete;
+
+namespace DataAccessLayer.Repositories
+{
+    public class CommentRepository : IGenericDAL<Comment>
+    {
+        public void Delete(Comment t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Comment GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<Comment> GetListAll()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Insert(Comment t)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(Comment t)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
